4bdb8ea290ff4a14ff45d3528768463f2cdc611d
Kevin <kevinkrupa13@vt.edu>
2021-01-15 21:07:14 -0500

    PlayerMove Script
    Player can now move, still have to add animations

---
+++ Maze Runner/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public Transform player;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // LateUpdate is called last once per frame
+    void LateUpdate()
+    {
+        if (transform.position != player.position) {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
