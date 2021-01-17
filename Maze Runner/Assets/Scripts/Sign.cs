017a825b17156361ad1ff0fda525ace49c38004f
Kevin <kevinkrupa13@vt.edu>
2021-01-17 02:03:22 -0500

    Work on the sign script
    Signs almost done, just need to work on the script

---
+++ Maze Runner/Assets/Scripts/Sign.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool inRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRage) {
+            if (dialogBox.activeInHierarchy) {
+                dialogBox.SetActive(false);
+            }
+            else {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D player) {
+        
+        if(player.CompareTag("Player")) {
+            inRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D player) {
+        
+        if(player.CompareTag("Player")) {
+            inRange = false;
+            dialogBox.SetActive(false);
+        }
+
+    }
+}
