bb69363f35860af2b552e776949063e3f1570adf
Kevin Krupa <kevinkrupa13@vt.edu>
2021-01-21 01:19:44 -0500

    Working on zombie movement
    Zombie moves but only from command. Also animations need work

---
+++ Maze Runner/Assets/Scripts/Zombie.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ZombieState
+{
+    walk,
+    attack
+}
+
+public class Zombie : MonoBehaviour
+{
+
+    public ZombieState state;
+    public float speed;
+    public GameObject player;
+    private Vector3 change;
+    private Vector3 playerVector;
+    private Rigidbody2D myRigidbody;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerVector = Vector3.zero;
+        change = Vector3.zero;
+
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (state == ZombieState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
